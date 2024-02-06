f3213b922de23ba1982e4ec6be00e1c453c8af09
im-Arn <110503940+im-Arn@users.noreply.github.com>
2024-02-06 09:34:47 +0300

    Alg.Sprint1 Rev.1

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,142 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
+import Style from "./queue-page.module.css";
+import Queue from "./queue";
+//ui
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+//types
+import { ElementStates } from "../../types/element-states";
+import { TQueue, DisableButtonState, LoadersState } from "../../types/types";
+//const
+import { SHORT_DELAY_IN_MS, setDelay } from "../../constants/delays";
 
 export const QueuePage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [queue, setQueue] = useState(new Queue<string>(7));
+  const [elements, setElements] = useState<(TQueue | undefined)[]>(queue.elements().map((elem) => ({ elem, state: false })));
+  const [headIndex, setHeadIndex] = useState<number | null>(null);
+  const [tailIndex, setTailIndex] = useState<number | null>(null);
+  const [disableButton, setDisableButton] = useState<DisableButtonState>({
+    addButton: true,
+    deleteButton: true,
+    clearButton: true,
+  });
+  const [loaders, setLoaders] = useState<LoadersState>({
+    addButton: false,
+    deleteButton: false,
+    clearButton: false,
+  });
+
+  useEffect(() => {
+    setDisableButton({
+      addButton: inputValue.trim().length === 0,
+      deleteButton: queue.getLength() === 0,
+      clearButton: queue.getLength() === 0,
+    });
+  }, [inputValue, elements]);
+
+  const updateQueueVisualization = async (action: () => void, buttonType: "addButton" | "deleteButton" | "clearButton") => {
+    setLoaders((prevLoaders) => ({ ...prevLoaders, [buttonType]: true }));
+    action();
+    await setDelay(SHORT_DELAY_IN_MS);
+    setLoaders((prevLoaders) => ({ ...prevLoaders, [buttonType]: false }));
+  };
+
+  const renderQueue = async (button: "addButton" | "deleteButton", value?: string) => {
+    if (button === "addButton" && value) {
+      queue.enqueue(value);
+    }
+    const newQueue: TQueue[] = [
+      ...queue.getElements().map((elem) => ({ elem, state: false })), //мутировали массив из класса
+    ];
+
+    if (button === "addButton" && value) {
+      newQueue[queue.getTail() - 1] = { ...newQueue[queue.getTail() - 1], state: true, }; //покрасили последний эл-т
+    }
+    else {
+      newQueue[queue.getHead()] = { ...newQueue[queue.getHead()], state: true, }; //или покрасили первый эл-т
+    }
+
+    setElements(newQueue); //отрисовали
+    await setDelay(SHORT_DELAY_IN_MS);
+    if (button === "addButton" && value) { setInputValue(""); } //обнулили инпут если это была операция добавления эл-та
+    else { queue.dequeue(); } //или запустили удаление
+
+    if (button === "addButton" && value) {
+      setElements((prevState) => prevState.map((elem) => ({ ...elem, state: false, }))); //перекрасили все обратно в неактивны
+      setTailIndex(queue.isEmpty() ? null : queue.getTail() - 1);
+    } else {
+      const temp: TQueue[] = [...queue.getElements().map((elem) => ({ elem, state: false }))];//мутировали массив после удаления элемента
+      setElements(temp);
+      setTailIndex(queue.isEmpty() ? null : queue.getTail() - 1);
+    }
+    setHeadIndex(queue.isEmpty() ? null : queue.getHead());
+  }
+
   return (
     <SolutionLayout title="Очередь">
-
+      <section>
+        <form className={Style.form}>
+          <div className={Style.group}>
+            <Input
+              placeholder="Введите значение"
+              maxLength={4}
+              isLimitText={true}
+              value={inputValue}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setInputValue(e.target.value);
+              }}
+            />
+            <Button
+              text="Добавить"
+              onClick={async () => {
+                updateQueueVisualization(() => {
+                  renderQueue("addButton", inputValue);
+                }, "addButton");
+                setInputValue("");
+              }}
+              disabled={tailIndex === 6 || disableButton.addButton}
+              isLoader={loaders.addButton}
+            />
+            <Button
+              text="Удалить"
+              onClick={async () => {
+                updateQueueVisualization(async () => {
+                  renderQueue("deleteButton");
+                }, "deleteButton");
+              }}
+              disabled={disableButton.deleteButton}
+              isLoader={loaders.deleteButton}
+            />
+          </div>
+          <Button
+            text="Очистить"
+            onClick={() => {
+              queue.clear();
+              setElements([...queue.getElements().map((elem) => ({ elem, state: false })),]);
+              setHeadIndex(null);
+              setTailIndex(null);
+            }}
+            disabled={disableButton.clearButton}
+          />
+        </form>
+        <ul className={Style.ul}>
+          {elements.map((item, index) => (
+            <li className={Style.listItem} key={index}>
+              <Circle
+                key={index}
+                index={index}
+                letter={item?.elem}
+                head={headIndex === index ? "head" : ""}
+                tail={tailIndex === index && !queue.isEmpty() ? "tail" : ""}
+                state={item?.state ? ElementStates.Changing : ElementStates.Default}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
     </SolutionLayout>
   );
 };
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,119 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
+import Style from "./stack-page.module.css";
+import Stack from "./stack";
+//ui
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+//types
+import { ElementStates } from "../../types/element-states";
+import { TStack, DisableButtonState, LoadersState } from "../../types/types";
+//const
+import { SHORT_DELAY_IN_MS, setDelay } from "../../constants/delays";
 
 export const StackPage: React.FC = () => {
+  const [stack] = useState(new Stack<string>());
+  const [stackElements, setStackElement] = useState<TStack[]>(
+    stack.elements.map((elem) => ({ elem, state: false }))
+  );
+  const [inputValue, setInputValue] = useState<string>("");
+  const [disableButton, setDisableButton] = useState<DisableButtonState>({
+    addButton: true,
+    deleteButton: true,
+    clearButton: true,
+  });
+  const [loaders, setLoaders] = useState<LoadersState>({
+    addButton: false,
+    deleteButton: false,
+    clearButton: false,
+  });
+
+  useEffect(() => {
+    setDisableButton({
+      addButton: inputValue.trim().length === 0,
+      deleteButton: stackElements.length === 0,
+      clearButton: stackElements.length === 0,
+    });
+  }, [inputValue, stackElements]);
+
+  const handleButtonClick = async (buttonType: "deleteButton" | "clearButton" | "addButton") => {
+    const loadersCopy = { ...loaders };
+    loadersCopy[buttonType] = true;
+    setLoaders(loadersCopy);
+
+    if (buttonType === "addButton") {
+      stack.push(inputValue);
+    } else if (buttonType === "deleteButton") {
+      stack.pop();
+    } else if (buttonType === "clearButton") {
+      stack.clear();
+    }
+
+    const newStackElements: TStack[] = stack.elements.map((elem, index) => ({
+      elem,
+      state: index === stack.elements.length - 1,
+    }));
+
+    setStackElement(newStackElements);
+    await setDelay(SHORT_DELAY_IN_MS);
+    setStackElement((prevNumbers) =>
+      prevNumbers.map((element) => ({ ...element, color: false }))
+    );
+
+    setInputValue("");
+
+    const loadersCopyAfterAnimation = { ...loaders };
+    loadersCopyAfterAnimation[buttonType] = false;
+    setLoaders(loadersCopyAfterAnimation);
+  };
+
   return (
     <SolutionLayout title="Стек">
-
+      <section>
+        <form className={Style.form}>
+          <div className={Style.container}>
+            <Input
+              maxLength={4}
+              isLimitText={true}
+              value={inputValue}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setInputValue(e.target.value);
+              }}
+            />
+            <Button
+              text="Добавить"
+              onClick={() => handleButtonClick("addButton")}
+              disabled={disableButton.addButton}
+              isLoader={loaders.addButton}
+            />
+            <Button
+              text="Удалить"
+              onClick={() => handleButtonClick("deleteButton")}
+              disabled={disableButton.deleteButton}
+              isLoader={loaders.deleteButton}
+            />
+          </div>
+          <Button
+            text="Очистить"
+            onClick={() => handleButtonClick("clearButton")}
+            disabled={disableButton.clearButton}
+          />
+        </form>
+        <ul className={Style.ul}>
+          {stackElements.map(({ elem, state }, index) => (
+            <li className={Style.listItem} key={index}>
+              <Circle
+                key={index}
+                letter={elem}
+                head={index === stackElements.length - 1 ? "top" : ""}
+                index={index}
+                state={state ? ElementStates.Changing : ElementStates.Default}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
     </SolutionLayout>
   );
 };
